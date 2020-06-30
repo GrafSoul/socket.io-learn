9d6e506f2165739c3aa401763e2285c63109d818
GrafSoul <grafsoul@gmail.com>
2020-06-30 13:42:46 +0500

    feat: Added an example of a simple chat

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 // app.get('/', (req, res) => {
 //     res.send('<h1> Hello world </ h1>');
@@ -7,6 +8,21 @@ var http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.broadcast.emit('hi');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+
+        console.log('message: ' + msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
